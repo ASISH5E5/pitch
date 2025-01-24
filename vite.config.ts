f1280e878ac858b51219ceab289a22e4c31bcc28
Asish <asishp5742@gmail.com>
2025-01-24 13:19:42 +0530

    Commit Message

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
- 
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"), // Alias for src directory
     },
   },
+  base: "/pitch/",  // GitHub Pages requires this base path
+  build: {
+    outDir: "dist",  // Make sure the build output is in the 'dist' folder
+  },
 })
-
