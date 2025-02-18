dbe9811ec9450cf5f8088582178bc832ccb4704b
PYDI ASISH KUMAR <21PA1A05E5@VISHNU.EDU.IN>
2025-02-18 23:29:59 +0530

    Message

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
     return( 
     <BrowserRouter basename="/pitch/">
     <Routes>
-      <Route path="/pitch/" element={<Home/>}/>
+      <Route path="/" element={<Home/>}/>
     <Route path="/main" element={<Main data={false}/>}/>
     <Route path="*" element={<NotFound />} />
     
